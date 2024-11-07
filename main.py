5784779630d66c5441b78ccce1280a1ea4fb83b9
SKonstantinA <skakalka.man@yandex.ru>
2024-11-08 02:12:42 +0300

    lesson 2

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/hello")
-print(response.text)
+# payload = {"name": "User"}
+# response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+# print(response.text)
+
+response = requests.get("https://playground.learnqa.ru/api/hello", params={"name": "User"})
+parsed_response_text = response.json()
+print(parsed_response_text["answer"])
---
+++ status_code.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.post("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+# print(response.status_code)
+# print(response.text)
+
+first_response = response.history[0]
+second_response = response
+
+print(first_response.url)
+print(second_response.url)
